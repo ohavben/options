b7fd170ccf5a76a5642052894e05bd220c0543d0
Ohav Ben Yanai <12668619+ohavben@users.noreply.github.com>
2022-07-17 10:49:06 -0700

    Update options.cpp

--- options.cpp
+++ options.cpp
@@ -449,36 +449,3 @@ namespace
 
   EMSCRIPTEN_BINDINGS(quantlib) { emscripten::function("calcuateOption", &calcuateOption); }
 }
-/*
-int main(int, char* []) {
- 
-    try {
-  
-      json j_data = R"({
-        "executionStyle":0,
-        "todaysDate":"1998-05-15",
-        "settlementDate": "1998-05-17",
-        "maturityDate": "1999-05-17",
-        "optionType":-1,
-        "underlying": 36,
-        "strike": 40,
-        "dividendYield": 0,
-        "riskFreeRate": 0.06,
-        "optionPrice": 4.00
-      })"_json;
-
-      std::string result = calcuateOption(j_data.dump());
-
-      std::cout << "Result: "  << result << std::endl;
-        
-      return 0;
- 
-    } catch (exception& e) {
-        cerr << e.what() << endl;
-        return 1;
-    } catch (...) {
-        cerr << "unknown error" << endl;
-        return 1;
-    }
-}
-*/
